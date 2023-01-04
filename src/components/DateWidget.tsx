e28b8e4235649834e3451f3aa37e44a709ad28cc
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2023-01-04 14:19:15 +0100

    fix

--- src/components/DateWidget.tsx
+++ src/components/DateWidget.tsx
@@ -5,7 +5,6 @@ export const DateWidget = () => {
     useEffect(() => {
        const dt = new Date();
        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        console.log(dt.toLocaleDateString('it-IT', options));
     }, [])
   return <></>;
 };
