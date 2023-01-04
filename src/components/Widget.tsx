8dd700b705990dd953e3d30eaeb1f3e2d8f2f380
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2023-01-04 16:48:59 +0100

    add home widget

---
+++ src/components/Widget.tsx
@@ -0,0 +1,56 @@
+import { Avatar, Button, Card, Grid, Typography } from "@mui/material";
+import { UserAuth } from "../context/AuthContext";
+import { DigitalClock } from "./DigitalClock";
+import { DigitalDate } from "./DigitalDate";
+import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
+import { MdOutlineVerified } from "react-icons/md";
+export const Widget = () => {
+  const { user, logOut } = UserAuth();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="widget-container">
+      <h1 className="display-4">Widget</h1>
+      <Grid container direction="column" columns={1}>
+        <Card variant="outlined" style={{ marginTop: "1rem" }}>
+          <div className="card-prof">
+            <img
+              alt={user.displayName}
+              height="50px"
+              referrerPolicy="no-referrer"
+              src={user.photoURL.replace("s96", "s150")}
+            />
+            <Typography variant="h5" gutterBottom>
+              {user.displayName} {!user.isAnonymous && <MdOutlineVerified />}
+            </Typography>
+
+            <Button
+              variant="outlined"
+              startIcon={<ExitToAppOutlinedIcon />}
+              onClick={handleLogout}
+            >
+              LOGOUT
+            </Button>
+          </div>
+        </Card>
+
+        <Card variant="outlined" style={{ marginTop: "1rem" }}>
+          <div className="card-dt">
+            <p className="lead">Schedule</p>
+
+            <DigitalClock format24={false} />
+            <p className="lead">Date</p>
+
+            <DigitalDate />
+          </div>
+        </Card>
+      </Grid>
+    </div>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
+import { Box } from "@mui/material";
+import { Widget } from "../components/Widget";
+import { useWindowResize } from "../hooks/useWindowsSize";
 import { Main } from "./Main";
 
 export const Home = () => {
-    return (
-      <div>
-        <Main />
-      </div>
-    );
-  };
-  
+  const width = useWindowResize();
+  const brackPoint = 1120;
+  return (
+    <div>
+      <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+        <Box gridColumn={width > brackPoint ? "span 8" : "span 12"}>
+            <Main />
+        </Box>
+        { width > brackPoint &&  <Box gridColumn="span 4">
+          <Widget />
+        </Box>}
+      </Box>
+    </div>
+  );
+};
